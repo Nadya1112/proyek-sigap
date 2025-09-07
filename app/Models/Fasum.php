5abc4cb8069da5e75acbfa15a7ccc454703c6be3
Farhan Basahir <farhanbasahir@gmail.com>
2025-09-08 06:55:42 +0800

    Farhan - melakukan remove filament sementara

--- app/Models/Fasum.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Fasum extends Model
-{
-    protected $fillable = ['nama_perumahan','alamat','status_sertifikat','tanggal_serah','keterangan'];
-}
--- app/Models/Pengaduan.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Pengaduan extends Model
-{
-    protected $fillable = ['nama','kontak','isi','bukti_path','status'];
-}
